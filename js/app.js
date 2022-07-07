f109d046cf18ef54d7d95951b0fc6e1c4084910e
nguymina <mnguyen27639@gmail.com>
2022-07-06 18:15:14 -0600

    put in placeholder html

--- js/app.js
+++ js/app.js
@@ -63,16 +63,74 @@ class Tamagotchi {
     eatUp () {
         this.hunger -= 1;
     };
+    getHungry () {
+        this.hunger += 1;
+    };
     sleepPls () {
         this.sleepiness = 0;
         //add something to increment the decrease once time cycle is created
     };
+    getSleepy () {
+        this.sleepiness += 1;
+    };
     playBall () {
         this.boredom -= 1;
     };
+    getBored () {
+        this.boredom += 1;
+    }
     ageUp () {
         this.age += 1;
     };
     // Add clean, buy meds, make money??
 };
 
+//Test Tamagotchi + Name input
+
+//add button for start game pop up prompt
+const startGame = () => {
+    let nameThis = prompt("What's my name?");
+    tama1 = new Tamagotchi(nameThis);
+};
+
+
+//Time 
+
+//setTimeOut or setInterval or maybe just for loop?
+let time = 0;
+
+const timeElapsed = () => {
+    time += 1;
+};
+
+const timePassing = () => {
+    setInterval(timeElapsed(), 1000);
+
+    //Increase values based on time
+    if (time % 10 === 0) {
+        getBored();
+        getHungry();
+    };
+    if (time % 20 === 0) {
+        getSleepy();
+    };
+
+    //Stages of growth
+    if (time === 120) {
+        tama1.status = "Child";
+    }
+    if (time === 300) {
+        tama1.status = "Teenager";
+    }
+    if (time === 600) {
+        tama1.status = "Adult";
+    };
+    if (time === 900) {
+        tama1.status = "Dead";
+        //create Death + reset game function
+    };
+
+};
+
+
+// setInterval only works passes one function, so multiple set interval functions are needed
